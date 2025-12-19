9c57bdb2e084c471e8433498a87b40ada3831b20
pestabruno <bruno.pestana187735@alumnos.info.unlp.edu.ar>
2025-12-19 13:32:47 -0300

    fixxs

--- front-donde-esta-mascota/src/app/components/home/home.ts
+++ front-donde-esta-mascota/src/app/components/home/home.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { faDog, faCat } from '@fortawesome/free-solid-svg-icons'; // <-- Importamos los íconos
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MapComponent } from '../map/map'; // Importación
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
-  imports: [FontAwesomeModule, MapComponent],
+
+  imports: [FontAwesomeModule, MapComponent, RouterLink, RouterLinkActive],
 })
 export class HomeComponent implements OnInit {
   tituloPrincipal: string = 'Encuentra a tu Mascota Perdida';
